a59c2202a767a43b37826be5cbcc0f12078b2b1f
Emanuele Fasulo <e.fasulo@reply.it>
2025-07-15 21:59:29 +0200

    rimozioni file non aggiornati

--- src/main/java/com/financial/forYou/exception/AccountNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.financial.forYou.exception;
-
-public class AccountNotFoundException extends RuntimeException{
-    public AccountNotFoundException(String message, Throwable cause) {
-        super(message, cause);
-    }
-}
--- src/main/java/com/financial/forYou/repository/CardRepository.java
+++
@@ -1,7 +0,0 @@
-package com.financial.forYou.repository;
-
-import com.financial.forYou.model.Carta;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CardRepository extends JpaRepository<Carta, Long> {
-}
